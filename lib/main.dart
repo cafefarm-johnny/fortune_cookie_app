dfc196a72e1b18c3fe73983ff3761342b1f695cd
cafefarm-johnny <npcdja@gmail.com>
2022-04-09 23:23:50 +0900

    feat: FortuneCookie 위젯 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
-
 import 'home_page.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    // TODO: 18. DEBUG 배너를 제거해주세요. (Hot reload가 안되면 앱을 재시작해주세요.)
-    home: HomePage(),
-  ));
+  runApp(const FortuneCookie());
+}
+
+class FortuneCookie extends StatefulWidget {
+  const FortuneCookie({Key? key}) : super(key: key);
+
+  @override
+  State<FortuneCookie> createState() => _FortuneCookieState();
+}
+
+class _FortuneCookieState extends State<FortuneCookie> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // TODO: 18. DEBUG 배너를 제거해주세요. (Hot reload가 안되면 앱을 재시작해주세요.)
+      home: HomePage(),
+    );
+  }
 }
