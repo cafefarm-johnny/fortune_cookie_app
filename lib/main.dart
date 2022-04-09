6b3cb3deb6689c471fdf163d9322d910971d1887
cafefarm-johnny <npcdja@gmail.com>
2022-04-10 01:53:36 +0900

    style: App 클래스명 충돌 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(const FortuneCookie());
+  runApp(const FortuneCookieApp());
 }
 
-class FortuneCookie extends StatefulWidget {
-  const FortuneCookie({Key? key}) : super(key: key);
+class FortuneCookieApp extends StatefulWidget {
+  const FortuneCookieApp({Key? key}) : super(key: key);
 
   @override
-  State<FortuneCookie> createState() => _FortuneCookieState();
+  State<FortuneCookieApp> createState() => _FortuneCookieAppState();
 }
 
-class _FortuneCookieState extends State<FortuneCookie> {
+class _FortuneCookieAppState extends State<FortuneCookieApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
